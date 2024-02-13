12dd70f7a5ac2b7a25ce7c601987132f70a94d2b
ashisah <ashleysah@gmail.com>
2024-02-13 10:32:08 -0500

    added base code for traffic simulation

--- traffic_simulation.py
+++ traffic_simulation.py
@@ -1,6 +1,20 @@
 import pyglet
 
+window = pyglet.window.Window(width=800, height=600, caption='Traffic Simulation')
 
+@window.event
+def on_draw():
+    window.clear()
+    # Draw your simulation here
+
+def update(dt):
+    # Update simulation state
+    pass
+
+# Schedule the update function to be called every 60th of a second
+pyglet.clock.schedule_interval(update, 1/60.0)
+
+pyglet.app.run()
 
 
 
