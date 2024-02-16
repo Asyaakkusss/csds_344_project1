18ef984eb8c85dd620d6c54edf1a9dde6e51942c
ashisah <ashleysah@gmail.com>
2024-02-15 19:55:04 -0500

    trying to draw traffic lights onto image

--- traffic_simulation.py
+++ traffic_simulation.py
@@ -4,6 +4,12 @@ import pyglet
 image = pyglet.resource.image('Typical-traffic-light-system-at-a-four-way-intersection.png')
 window = pyglet.window.Window(width = image.width, height=image.height, caption='Traffic Simulation')
 
+green_light = pyglet.image.load('../resources/traffic_light_green.png')
+#red_light = pyglet.image.load('/resources/traffic_light_yellow.png')
+#yellow_light = pyglet.image.load('/resources/traffic_light_red.png')
+
+green_sprite = pyglet.sprite.Sprite(green_light, x= 100, y =100)
+
 @window.event
 def on_draw():
     window.clear()
@@ -14,8 +20,13 @@ def update(dt):
     # Update simulation state
     pass
 
+def ns_green_update(dt):
+    green_sprite.draw()
+    pass
+
 # Schedule the update function to be called every 60th of a second
 pyglet.clock.schedule_interval(update, 1/60.0)
+#pyglet.clock.schedule_interval(ns_green_update, 1/60.0)
 
 pyglet.app.run()
 
