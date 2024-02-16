e07f31c221472197009aa7d50929b3b40e27928a
Asya <aya29@case.edu>
2024-02-15 20:11:36 -0500

    green traffic light placement refined

--- traffic_simulation.py
+++ traffic_simulation.py
@@ -1,19 +1,31 @@
+import sys
+sys.path.append(r'C:\Users\asyas\AppData\Local\Programs\Python\Python311\Lib\site-packages')
 import pyglet
 
 
 image = pyglet.resource.image('Typical-traffic-light-system-at-a-four-way-intersection.png')
 window = pyglet.window.Window(width = image.width, height=image.height, caption='Traffic Simulation')
 
-green_light = pyglet.image.load('../resources/traffic_light_green.png')
+green_light = pyglet.image.load('traffic_light_green.png')
 #red_light = pyglet.image.load('/resources/traffic_light_yellow.png')
 #yellow_light = pyglet.image.load('/resources/traffic_light_red.png')
 
-green_sprite = pyglet.sprite.Sprite(green_light, x= 100, y =100)
+green_sprite_west = pyglet.sprite.Sprite(green_light, x= 100, y =125)
+#green_sprite_west.rotation += 90
+green_sprite_east = pyglet.sprite.Sprite(green_light, x= 285, y =250)
+green_sprite_north = pyglet.sprite.Sprite(green_light, x= 140, y =325)
+green_sprite_north.rotation += 90
+green_sprite_south = pyglet.sprite.Sprite(green_light, x= 285, y =150)
+green_sprite_south.rotation += 90
 
 @window.event
 def on_draw():
     window.clear()
     image.blit(0, 0)
+    green_sprite_west.draw()
+    green_sprite_east.draw()
+    green_sprite_north.draw()
+    green_sprite_south.draw()
     # Draw your simulation here
 
 def update(dt):
