465fc625642aa39b08105a0f9274f99e506738f0
Asya <aya29@case.edu>
2024-02-16 20:32:48 -0500

    traffic light going from green to red to yellow and oscillates between red and yellow

--- traffic_simulation.py
+++ traffic_simulation.py
@@ -3,14 +3,15 @@ import sys
 sys.path.append(r'C:\Users\asyas\AppData\Local\Programs\Python\Python311\Lib\site-packages')
 import pyglet
 
+#main image generated here 
 image = pyglet.resource.image('Typical-traffic-light-system-at-a-four-way-intersection.png')
 window = pyglet.window.Window(width = image.width, height=image.height, caption='Traffic Simulation')
 
+#green and yellow light images generated here 
 green_light = pyglet.image.load('traffic_light_green.png')
-green_light.id = 'green light'
-#red_light = pyglet.image.load('/resources/traffic_light_yellow.png')
 yellow_light = pyglet.image.load('traffic_light_yellow.png')
-yellow_light.id = 'yellow light'
+red_light = pyglet.image.load('traffic_light_red.png')
+
 
 sprite_west = pyglet.sprite.Sprite(green_light, x= 100, y =125)
 sprite_east = pyglet.sprite.Sprite(green_light, x= 285, y =250)
@@ -18,24 +19,28 @@ sprite_north = pyglet.sprite.Sprite(green_light, x= 140, y =325)
 sprite_north.rotation += 90
 sprite_south = pyglet.sprite.Sprite(green_light, x= 285, y =150)
 sprite_south.rotation += 90
-sprite_south.image.id = green_light.id
 
 
 def ns_green_update(dt):
-    
+    green_light = True 
     path = 'traffic_light_green.png'
-    if (sprite_south.image.id == green_light.id):
+    if green_light:
         path = 'traffic_light_yellow.png'
-    if (sprite_south.image.id == yellow_light.id): 
-        path = 'traffic_light_red.png'
-    
-    
+
     new_image = pyglet.resource.image(path)
     sprite_south.image = new_image
-    pass
 
+def ns_yellow_update(dt): 
+    yellow_light = True
+    path = 'traffic_light_yellow.png'
+    if yellow_light: 
+        path = 'traffic_light_red.png'
+    new_image = pyglet.resource.image(path)
+    sprite_south.image = new_image
+    
 # Schedule the update function to be called every 60th of a second
 pyglet.clock.schedule_interval(ns_green_update, 3)
+pyglet.clock.schedule_interval(ns_yellow_update, 3)
 #pyglet.clock.schedule_interval(ns_green_update, 1/60.0)
 
 @window.event
